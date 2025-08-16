649eaa2ea9003eff8215ca88866a6ead7a9e1c3d
thanhbeo0 <lyvanthan16049@gmail.com>
2025-08-16 09:33:58 +0700

    Update Atwo.lua

--- Scripts/Lua/Atwo.lua
+++ Scripts/Lua/Atwo.lua
@@ -15,6 +15,10 @@ function cache.Funcrandom(length)
 end
 
 cache.Player = game:GetService("Players").LocalPlayer
+
+cache.candestroy = false
+cache.UserInputService = game:GetService("UserInputService")
+
 cache.PlayerGui = cache.Player:WaitForChild("PlayerGui")
 cache.ScreenGui = Instance.new("ScreenGui")
 cache.ScreenGui.Parent = cache.PlayerGui
@@ -31,13 +35,40 @@ function m.InitGui(c)
   win.BackgroundColor3 = Color3.fromRGB(160,160,160)
   win.BackgroundTransparency = 0.4
   
+  win.BorderSizePixel = 4
+
+  win.BorderColor3 = Color3.fromRGB(0,0,0)
+  
+  win.Visible = false
+
+  local ButtonOpen = Instance.new("TextButton")
+  ButtonOpen.Parent = cache.ScreenGui
+  ButtonOpen.Size = UDim2.new(0,50,0,25)
+  ButtonOpen.Position = UDim2.new(0,0,0.5,0)
+  ButtonOpen.AnchorPoint = Vector2.new(0,0.5)
+  ButtonOpen.BackgroundColor3 = Color3.fromRGB(165,165,165)
+  ButtonOpen.Text = "OPEN"
+
+  ButtonOpen.Activated:Connect(function()
+    ButtonOpen.Text = (ButtonOpen.Text == "OPEN") and "CLOSE" or "OPEN"
+    win.Visible = not win.Visible
+  end)
+
   cache.Window = win
+  cache.ButtonOpen = ButtonOpen
+
+  cache.candestroy = true
 end
 
-function m.DestroyWindow()
-  if cache.Window ~= nil then
+function m.Destroy()
+  if cache.candestroy then
     cache.Window:Destroy()
     cache.Window = nil
+
+    cache.ButtonOpen:Destroy()
+    cache.ButtonOpen = nil
+
+    cache.candestroy = false
   end
 end
 
