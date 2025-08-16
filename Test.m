1531264cd95bc3b5089595f69b98efa52be688f5
thanhbeo0 <lyvanthan16049@gmail.com>
2025-08-16 11:01:41 +0700

    Create Test.m

---
+++ Test.m
@@ -0,0 +1,31 @@
+#file status <package>
+#set(print) noautonewline (on)
+#remove(__main__)
+
+import package.Interface.*
+
+def m = new Module()
+
+def Gui = NULL;
+Gu
+
+def m.addMember(Init:bool(){
+  wait Interface, Element;
+  Gui = new Interface.CreateWindow("MyApp", Element.Size.Balance, Element.Size.Balance, Element.Position.Balance, Element.Position.Balance, Element.ColorRGB(155,155,155))
+  if(Gui.Failed){
+    error("Create Window Failed\n");
+    return true;
+  }
+  return false;
+});
+
+def m.addMember(Destroy:bool(){
+  if (!Gui){
+    error("Gui is not init\n");
+    return true;
+  }
+  Interface.DestroyWindow(Gui);
+  return false;
+});
+
+export {m};
